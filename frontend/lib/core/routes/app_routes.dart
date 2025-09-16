d1a4413a40773f9f57d83c137f2e6597fc36da42
Matheus Hora <matheus11hora@gmail.com>
2025-09-16 11:15:43 -0300

    ✨ feat: adiciona a classe AppRoutes com definições de rotas para navegação

---
+++ frontend/lib/core/routes/app_routes.dart
@@ -0,0 +1,13 @@
+class AppRoutes {
+  static const String login = '/login';
+
+  static const String clients = '/clients';
+  static const String clientDetail = '/clients/:id';
+  static const String clientCreate = '/clients/create';
+  static const String clientEdit = '/clients/:id/edit';
+
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+
+  static const String splash = '/';
+}

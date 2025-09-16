815408af595bb7e304925fe293badad9d07b2fbd
Matheus Hora <matheus11hora@gmail.com>
2025-09-16 10:37:07 -0300

    ✨ feat: cria entidade User com propriedades e relacionamentos

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,53 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Client } from '../../clients/entities/client.entity';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column({
+    type: 'text',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+
+  @Column({ nullable: true })
+  provider: string;
+
+  @Column({ nullable: true })
+  lastLoginAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Client, (client) => client.assignedUser)
+  assignedClients: Client[];
+}

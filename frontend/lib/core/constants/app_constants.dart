180cb424236a1fcf82b6bd1386534c315684530a
Matheus Hora <matheus11hora@gmail.com>
2025-09-16 11:06:49 -0300

    ✨ feat: adiciona classe AppConstants com constantes do aplicativo

---
+++ frontend/lib/core/constants/app_constants.dart
@@ -0,0 +1,20 @@
+class AppConstants {
+  static const String appName = 'Conéctar';
+  static const String baseUrl = 'http://localhost:3010';
+
+  static const String authLogin = '/auth/login';
+  static const String authRegister = '/auth/register';
+  static const String users = '/users';
+  static const String clients = '/clients';
+
+  static const String tokenKey = 'auth_token';
+  static const String userKey = 'user_data';
+  static const String isLoggedInKey = 'is_logged_in_key';
+
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String homeRoute = '/home';
+  static const String clientsRoute = '/clients';
+  static const String clientFormRoute = '/clients/form';
+  static const String profileRoute = '/profile';
+}

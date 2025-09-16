eed34184e8f5f56b94239c7d76f3991d27ad6fee
Matheus Hora <matheus11hora@gmail.com>
2025-09-16 11:11:56 -0300

    ✨ feat: remove rotas de autenticação e usuários da classe AppConstants

--- frontend/lib/core/constants/app_constants.dart
+++ frontend/lib/core/constants/app_constants.dart
@@ -2,11 +2,6 @@ class AppConstants {
   static const String appName = 'Conéctar';
   static const String baseUrl = 'http://localhost:3010';
 
-  static const String authLogin = '/auth/login';
-  static const String authRegister = '/auth/register';
-  static const String users = '/users';
-  static const String clients = '/clients';
-
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
   static const String isLoggedInKey = 'is_logged_in_key';

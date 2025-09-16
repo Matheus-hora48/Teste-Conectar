383605dc20510380c9d0e1636dc97b3b1142b348
Matheus Hora <matheus11hora@gmail.com>
2025-09-16 20:11:04 -0300

    ✨ feat: adiciona caso de uso para login com Google e processamento de callback

---
+++ frontend/lib/features/auth/domain/usecases/google_login_usecase.dart
@@ -0,0 +1,37 @@
+import 'package:url_launcher/url_launcher.dart';
+import '../repositories/auth_repository.dart';
+import '../models/auth_response.dart';
+
+class GoogleLoginUseCase {
+  final AuthRepository _repository;
+
+  GoogleLoginUseCase(this._repository);
+
+  Future<void> initiateGoogleLogin() async {
+    try {
+      await openGoogleAuth();
+    } catch (e) {
+      throw Exception('Erro ao iniciar login com Google: $e');
+    }
+  }
+
+  Future<AuthResponse> processCallback(String token) async {
+    try {
+      return await _repository.processGoogleCallback(token);
+    } catch (e) {
+      throw Exception('Erro ao processar callback do Google: $e');
+    }
+  }
+
+  Future<void> openGoogleAuth() async {
+    const url = 'http://localhost:3010/auth/google';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(
+        Uri.parse(url),
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      throw Exception('Não foi possível abrir o navegador para autenticação');
+    }
+  }
+}
